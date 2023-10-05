17eba7b7ce926e827b7e505717d11fe43a054751
cryptocowboye <144955974+cryptocowboye@users.noreply.github.com>
2023-10-04 22:49:50 -0400

    Add rows and columns

---
+++ js-files/js-file.js
@@ -0,0 +1,17 @@
+const canvas = document.querySelector(".canvas")
+
+for (i = 1; i < 17; i++) {
+    let row = document.createElement("div");
+    row.classList.add("row")
+    row.classList.add(`row${i}`);
+    canvas.appendChild(row);
+    for (k = 1; k < 17; k++) {
+        let fetchRow = document.querySelector(`.row${i}`)
+        let box = document.createElement("div");
+        box.classList.add("column")
+        box.classList.add(`column${k}`)
+        fetchRow.appendChild(box)
+
+    }
+    
+}
